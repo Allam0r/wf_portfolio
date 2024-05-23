4ab9abb34fc269aea8ff4721e8352b2cfb8f7fb5
Allam0r <florian.wiart05@gmail.com>
2024-05-23 12:17:29 +0200

    test with data

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/********** TypedJS **********/
+
 const typed = new Typed(".typed", {
   strings: ["Développeur front-end."],
   typeSpeed: 100,
@@ -6,15 +8,64 @@ const typed = new Typed(".typed", {
   loop: true,
 });
 
-document.addEventListener("wheel", (event) => {
-  const sections = document.querySelectorAll(".section");
-  const currentSectionIndex = Array.from(sections).findIndex((section) => {
-    return section.getBoundingClientRect().top === 0;
-  });
+/********** TypedJS **********/
 
-  if (event.deltaY > 0 && currentSectionIndex < sections.length - 1) {
-    sections[currentSectionIndex + 1].scrollIntoView({ behavior: "smooth" });
-  } else if (event.deltaY < 0 && currentSectionIndex > 0) {
-    sections[currentSectionIndex - 1].scrollIntoView({ behavior: "smooth" });
-  }
+/********** Affichage dynamique **********/
+document.addEventListener("DOMContentLoaded", function () {
+  const url = "./assets/data/data.json";
+
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const container = document.getElementById("project_box");
+      if (!container) {
+        throw new Error("Container element not found");
+      }
+
+      data.forEach((item) => {
+        // Créez la div de la carte
+        const cardDiv = document.createElement("div");
+        cardDiv.className = "card";
+
+        // Créez la div card_part1
+        const cardPart1 = document.createElement("div");
+        cardPart1.className = "card_part1";
+        const itemImg = document.createElement("img");
+        itemImg.src = `assets/img/${item.img}`;
+        itemImg.alt = `Aperçu page ${item.name}`;
+        cardPart1.appendChild(itemImg);
+
+        // Créez la div card_part2
+        const cardPart2 = document.createElement("div");
+        cardPart2.className = "card_part2";
+        const itemName = document.createElement("h2");
+        itemName.textContent = item.name;
+        const itemDesc = document.createElement("p");
+        itemDesc.textContent = item.desc;
+        const itemLink = document.createElement("a");
+        itemLink.href = "#"; // Remplacez par l'URL correcte si nécessaire
+        itemLink.textContent = "Live demo";
+
+        cardPart2.appendChild(itemName);
+        cardPart2.appendChild(itemDesc);
+        cardPart2.appendChild(itemLink);
+
+        // Ajoutez card_part1 et card_part2 à la carte
+        cardDiv.appendChild(cardPart1);
+        cardDiv.appendChild(cardPart2);
+
+        // Ajoutez la carte au conteneur
+        container.appendChild(cardDiv);
+      });
+    })
+    .catch((error) => {
+      console.error("Erreur lors du chargement du fichier JSON:", error);
+    });
 });
+
+/********** Affichage dynamique **********/
